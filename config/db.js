a2b722e732cd86e98c93347b3b2c70bec984d064
Evelyn Nicol Cettour <cettour53@gmail.com>
2026-07-21 18:56:30 -0300

    Limpiar y corregir db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
- connectionString: process.env.DATABASE_URL,
-    rejectUnauthorized: false
-  }
-});
-
-pool.query('SELECT 1', (err, res) => {
-  if (err) {
-    console.error("❌ ERROR CRÍTICO AL CONECTAR:", err.message);
-  } else {
-    console.log("✅ CONEXIÓN EXITOSA CON NEON");
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+    query: (text, params) => pool.query(text, params),
 };
